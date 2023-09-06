562c6416457d854bc3bff4631df0bcce0612d6ea
Oligarhselmasha <kirill-bulanov@yandex.ru>
2023-09-06 08:37:54 +0500

    ver 5

--- stat-service/client/src/main/java/ru/practicum/explorewithme/StatClient.java
+++ stat-service/client/src/main/java/ru/practicum/explorewithme/StatClient.java
@@ -8,8 +8,6 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
-import java.util.List;
-
 @Service
 public class StatClient extends BaseClient {
 
