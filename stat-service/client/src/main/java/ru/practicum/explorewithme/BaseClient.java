fb8818f7ad8c97aa65a65f954694dd9a40aa0b97
Oligarhselmasha <kirill-bulanov@yandex.ru>
2023-09-17 15:43:51 +0500

    ver 1

--- stat-service/client/src/main/java/ru/practicum/explorewithme/BaseClient.java
+++ stat-service/client/src/main/java/ru/practicum/explorewithme/BaseClient.java
@@ -1,6 +1,5 @@
 package ru.practicum.explorewithme;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.*;
 import org.springframework.lang.Nullable;
 import org.springframework.web.client.HttpStatusCodeException;
