bcd5dedb5033a07dcb0d99e7c78667f0ec046beb
Oligarhselmasha <kirill-bulanov@yandex.ru>
2023-09-27 00:54:24 +0500

    ver 3

---
+++ stat-service/server/src/main/java/ru/practicum/explorewithme/controller/ErrorHandler.java
@@ -0,0 +1,29 @@
+package ru.practicum.explorewithme.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.explorewithme.errorsHandling.ErrorResponse;
+import ru.practicum.explorewithme.exceptions.UnCorrectableException;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static variables.Constants.DATE_PATTERN;
+
+
+@RestControllerAdvice
+public class ErrorHandler {
+    private final DateTimeFormatter df = DateTimeFormatter.ofPattern(DATE_PATTERN);
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleIUnCorrectableException(final UnCorrectableException ex) {
+        return new ErrorResponse("BAD_REQUEST",
+                "Incorrectly made request.",
+                ex.getMessage(),
+                LocalDateTime.now().format(df)
+        );
+    }
+}
